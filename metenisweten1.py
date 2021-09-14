3889b0ae9f6201959384678b04d966d909bb4159
Svengeelhoed <99069588@mydavinci.nl>
2021-09-14 10:14:58 +0200

    elif statement

--- metenisweten1.py
+++ metenisweten1.py
@@ -1,6 +1,9 @@
-B = 2
-A = 6
-
-if B < A :
- Max = A
+A = input("Noem een getal: ")
+B = input("Noem nog een getal: ")
+Max = A
+Min = B
+if B <= A :
  print("A is het grootste getal: " + str(Max))
+elif A <= B:
+ print("B is groter dan A: " + str(Min))
+ 
